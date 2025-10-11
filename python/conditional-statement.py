0618a0c3eb42331ad650176472b32f83d23651e6
Rahul Rajput <156775643+rahul1384r@users.noreply.github.com>
2025-10-11 11:40:46 +0530

    feat: Basics covered

---
+++ python/conditional-statement.py
@@ -0,0 +1,10 @@
+a = 5
+b = 10
+c = 2
+
+if(a > b and a > c):
+    print(a, "is greater")
+elif (b > a and b > c):
+    print(b, "is greater")
+elif(c > a and c > b):
+    print(c, "is greater")
--- python/data-types.py
+++ python/data-types.py
@@ -8,3 +8,19 @@ print(type(b))
 print(type(c))
 print(type(d))
 print(type(e))
+
+# List
+f = ["apple", "orange", "mango"]
+print(type(f))
+
+# Tuple
+g = ("apple", "orange", "mango")
+print(type(g))
+
+# Set
+h = {"apple", "orange", "mango"}
+print(type(h))
+
+# Frozen set
+i = frozenset({"apple", "orange", "mango"})
+print(type(i))

1e3711f39fd1f24bf4de59572f7933d0802d51a0
Rahul Rajput <156775643+rahul1384r@users.noreply.github.com>
2025-10-13 22:24:20 +0530

    feat: Built-in and Custom modules

---
+++ python/modules_basics.py
@@ -0,0 +1,14 @@
+import math
+print(math.sqrt(4))
+
+
+from math import sqrt
+print(sqrt(4))
+
+import math as m
+print(m.pi)
+
+
+# Custom module
+import utils.my_module as my_module
+print(my_module.greeting("Rahul"))

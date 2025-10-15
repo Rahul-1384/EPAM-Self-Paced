4a9af59434d414e2702fc1735a9b62044d228799
Rahul Rajput <156775643+rahul1384r@users.noreply.github.com>
2025-10-15 13:27:54 +0530

    feat: Lists and tuples

--- python/lists.py
+++ python/lists.py
@@ -1,2 +1,19 @@
 a = [1,2,3,4,5,6]
 print(a[0:4])
+
+
+# List Comprehension
+# Baisc generation
+squares = [x*x for x in range(1,6)]
+print(squares)
+
+# Filtering
+numbers = [1,2,3,4,5,6,7]
+new_list = [x for x in numbers if x%2 == 0]
+print(new_list)
+
+
+# Unpacking
+numbers = [10, 20, 30, 40, 50]
+a, b, c, *d = numbers
+print(a,b,c,d)
---
+++ python/tuples.py
@@ -0,0 +1,2 @@
+a = (1,2, "Rahul")
+print(a)
